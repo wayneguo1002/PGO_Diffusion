54d10dccee3105f36d05f771c77e0c591d52af86
gwh <ubtwq@student.kit.edu>
2024-10-21 19:58:09 +0200

    clean repo

--- pose_diffusion/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pose_diffusion_model import PoseDiffusionModel
-
-
-from .denoiser import Denoiser, TransformerEncoderWrapper
-from .gaussian_diffuser import GaussianDiffusion
-from .image_feature_extractor import MultiScaleImageFeatureExtractor
